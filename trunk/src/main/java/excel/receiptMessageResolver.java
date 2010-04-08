bb18e87a93edce96df7e34d4c47c1ea4af5df052
ncrash <ncrash@29d8afbe-39c0-11df-b79a-85257740e83b>
2010-04-08 01:53:49 +0000

    신규 - task 1: 데이터베이스 기초 데이터에 대한 테스트케이스 전략 http://192.168.192.237/redmine/issues/show/1
     - excel이 기술한 mms를 sms convert 한것 testcase 작성
    
    git-svn-id: svn://192.168.192.237/cbmax@11 29d8afbe-39c0-11df-b79a-85257740e83b

---
+++ trunk/src/main/java/excel/receiptMessageResolver.java
@@ -0,0 +1,37 @@
+package excel;
+
+public class receiptMessageResolver {
+
+	public static String convertOriginalSms(String mmsContent) {
+		// TODO mms문자를 sms문자 건별로 분리 해야함
+		// 처리는 mmsContent의 내용을 file을 읽듯이 한줄 한줄 읽어들어여서
+		// 한줄의 길이가 특정 길이보다 짧은 경우 한줄로 합치기
+		StringBuffer result = new StringBuffer();
+		String smsContentPerLine = new String();
+		
+		String[] split = mmsContent.split("\n");
+		for (int i = 0; i < split.length; i++) {
+			
+			if(split[i].getBytes().length > 50) {
+				result.append(split[i] + "\n");
+				smsContentPerLine = new String();
+			} else {
+				if(smsContentPerLine.length() > 0 && split[i].indexOf("[") > -1) {
+					result.append(smsContentPerLine + "\n");
+					smsContentPerLine = new String();
+				} else {
+					if(smsContentPerLine.length() > 0) {
+						smsContentPerLine += " ";
+					}
+				}
+				smsContentPerLine += split[i];
+			}
+		}
+		
+		if(smsContentPerLine.length() > 0) {
+			result.append(smsContentPerLine + "\n");
+		}
+		
+		return result.toString();
+	}
+}
