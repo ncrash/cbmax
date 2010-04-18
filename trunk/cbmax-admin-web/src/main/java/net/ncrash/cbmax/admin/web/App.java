e55ba460b6f73c2915f9c23f05c560aee5972f0c
ncrash <ncrash@29d8afbe-39c0-11df-b79a-85257740e83b>
2010-04-18 11:38:07 +0000

    cbmax project maven module 구조 적용
    Share project "cbmax-admin-web" into "svn://192.168.192.237"
    Share project "cbmax-web" into "svn://192.168.192.237"
    Share project "cbmax" into "svn://192.168.192.237"
    Share project "cbmax-core" into "svn://192.168.192.237"
    
    
    git-svn-id: svn://192.168.192.237/cbmax@24 29d8afbe-39c0-11df-b79a-85257740e83b

---
+++ trunk/cbmax-admin-web/src/main/java/net/ncrash/cbmax/admin/web/App.java
@@ -0,0 +1,13 @@
+package net.ncrash.cbmax.admin.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
