7ba35eecaa38cd206c094100659702ca9692015a
ncrash <ncrash@29d8afbe-39c0-11df-b79a-85257740e83b>
2010-07-30 10:18:44 +0000

    gmail sms 분석 작업 중  - 압축파일 해제를 위한 cayenne 라이브러리 추가  - List 객체 assertion을 위해 unitils 라이브러리 추가
    git-svn-id: svn://192.168.192.237/cbmax@164 29d8afbe-39c0-11df-b79a-85257740e83b

--- trunk/cbmax/core-api/src/test/java/net/ncrash/cbmax/core/creditcard/GmailSmsFixtureTest.java
+++ trunk/cbmax/core-api/src/test/java/net/ncrash/cbmax/core/creditcard/GmailSmsFixtureTest.java
@@ -1,9 +1,20 @@
 package net.ncrash.cbmax.core.creditcard;
 
+import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals;
+import static org.unitils.reflectionassert.ReflectionComparatorMode.LENIENT_ORDER;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * cbmax maildir 압축파일로 테스트
@@ -15,6 +26,9 @@ import org.junit.BeforeClass;
  */
 public class GmailSmsFixtureTest {
 
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -31,4 +45,22 @@ public class GmailSmsFixtureTest {
 	public void tearDown() throws Exception {
 	}
 
+	@Test
+	public void testZipFileExtract() throws Exception {
+		File testFixtureExcelFile = new File(getClass().getClassLoader().getResource("gmail-archive.zip").getFile());
+		File extractFolder = folder.newFolder("temp");
+		String[] fileList = new String[] {"gmail-backup.mbox", "cur", "new", "tmp"};
+		List<String> destinationFileList = new ArrayList<String>();
+		
+		org.apache.cayenne.util.ZipUtil.unzip(testFixtureExcelFile, extractFolder);
+
+		// unzip 후 파일 목록을 비교해서 정상적으로 압축이 풀렸는지 검사
+		File destinationFile = new File(extractFolder.getPath() + "/" + "gmail-archive");
+		
+		for (File file : destinationFile.listFiles()) {
+			destinationFileList.add(file.getName());
+		}
+		
+		assertReflectionEquals(Arrays.asList(fileList), destinationFileList, LENIENT_ORDER);
+	}
 }
