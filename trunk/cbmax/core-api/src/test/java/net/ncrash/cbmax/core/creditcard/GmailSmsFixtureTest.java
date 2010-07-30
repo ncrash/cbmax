17d671abb8200c3423e6de4f69a0c059a025f2fd
ncrash <ncrash@29d8afbe-39c0-11df-b79a-85257740e83b>
2010-07-30 09:27:26 +0000

    gmail maildir 내용 테스트 케이스 작성 시작
    git-svn-id: svn://192.168.192.237/cbmax@163 29d8afbe-39c0-11df-b79a-85257740e83b

---
+++ trunk/cbmax/core-api/src/test/java/net/ncrash/cbmax/core/creditcard/GmailSmsFixtureTest.java
@@ -0,0 +1,34 @@
+package net.ncrash.cbmax.core.creditcard;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ * cbmax maildir 압축파일로 테스트
+ * 테스트시 junit4의 temp directory에 압축파일을 해제해서 테스트
+ *  
+ * @author daekwon.kang
+ * @since 2010. 7. 30.
+ * @see
+ */
+public class GmailSmsFixtureTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
